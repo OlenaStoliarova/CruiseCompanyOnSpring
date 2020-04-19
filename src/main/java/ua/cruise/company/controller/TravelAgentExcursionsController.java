fe0f7ded80fce1045b83a37f9b23bdd228216bb7
Olena Stoliarova <stoliarova.elena@gmail.com>
2020-04-19 13:31:56 +0300

    clean code of TravelAgentExcursionsController

--- src/main/java/ua/cruise/company/controller/TravelAgentExcursionsController.java
+++ src/main/java/ua/cruise/company/controller/TravelAgentExcursionsController.java
@@ -16,7 +16,6 @@ import ua.cruise.company.service.ExcursionService;
 import ua.cruise.company.service.SeaportService;
 import ua.cruise.company.service.exception.NoEntityFoundException;
 import ua.cruise.company.service.exception.NonUniqueObjectException;
-import ua.cruise.company.service.exception.SomethingWentWrongException;
 
 import javax.validation.Valid;
 
@@ -49,7 +48,6 @@ public class TravelAgentExcursionsController {
     @GetMapping("/{excursionId}/edit")
     public String showEditExcursionForm(@PathVariable Long excursionId,
                                         Model model) {
-        model.addAttribute("excursionId", excursionId);
         Excursion excursion;
         try {
             excursion = excursionService.getExcursionById(excursionId);
@@ -68,38 +66,16 @@ public class TravelAgentExcursionsController {
                                           @Valid @ModelAttribute("excursion") ExcursionForm excursionForm,
                                           BindingResult bindingResult,
                                           Model model) {
-
         LOGGER.info("editing excursion {}, new data {}", excursionId, excursionForm);
-        if (bindingResult.hasErrors()) {
-            LOGGER.error("Validation error: " + bindingResult.getFieldErrors());
-            model.addAttribute("validation_errors", true);
-            model.addAttribute("excursionId", excursionId);
-            setModelAttributesForExcursionForm(model, excursionForm);
-            return "/travel_agent/edit_excursion";
-        }
 
-        Excursion excursion = new ExcursionFormMapper().mapToEntity(excursionForm);
-        excursion.setId(excursionId);
-
-        try {
-            Long portId = excursionForm.getSeaportId();
-            excursion.setSeaport(seaportService.getPortById(portId));
-            excursionService.edit(excursion);
-            return "redirect:/travel_agent/excursions?seaportId=" + portId;
-
-        } catch (NoEntityFoundException e) {
-            LOGGER.error(e.getMessage());
-            model.addAttribute("no_port_found", true);
-        } catch (NonUniqueObjectException e) {
-            LOGGER.error(e.getMessage());
-            model.addAttribute("non_unique", true);
-        } catch (SomethingWentWrongException e) {
-            LOGGER.error(e.getMessage());
-            model.addAttribute("error", true);
+        if (hasNoValidationErrors(bindingResult, model)) {
+            if (saveExcursion(excursionForm,
+                    excursion -> excursionService.edit(excursion),
+                    model))
+                return "redirect:/travel_agent/excursions?seaportId=" + excursionForm.getSeaportId();
         }
 
         setModelAttributesForExcursionForm(model, excursionForm);
-        model.addAttribute("excursionId", excursionId);
         return "/travel_agent/edit_excursion";
     }
 
@@ -110,36 +86,17 @@ public class TravelAgentExcursionsController {
         return "/travel_agent/add_excursion";
     }
 
-
     @PostMapping("/add")
     public String submitAddExcursionForm(@Valid @ModelAttribute("excursion") ExcursionForm excursionForm,
-                                         BindingResult bindingResult, Model model) {
-
+                                         BindingResult bindingResult,
+                                         Model model) {
         LOGGER.info("adding excursion: " + excursionForm);
-        if (bindingResult.hasErrors()) {
-            LOGGER.error("Validation error: " + bindingResult.getFieldErrors());
-            model.addAttribute("validation_errors", true);
-            setModelAttributesForExcursionForm(model, excursionForm);
-            return "/travel_agent/add_excursion";
-        }
 
-        Excursion excursion = new ExcursionFormMapper().mapToEntity(excursionForm);
-
-        try {
-            Long portId = excursionForm.getSeaportId();
-            excursion.setSeaport(seaportService.getPortById(portId));
-            excursionService.create(excursion);
-            return "redirect:/travel_agent/excursions?seaportId=" + portId;
-
-        } catch (NoEntityFoundException e) {
-            LOGGER.error(e.getMessage());
-            model.addAttribute("no_port_found", true);
-        } catch (NonUniqueObjectException e) {
-            LOGGER.error(e.getMessage());
-            model.addAttribute("non_unique", true);
-        } catch (SomethingWentWrongException e) {
-            LOGGER.error(e.getMessage());
-            model.addAttribute("error", true);
+        if (hasNoValidationErrors(bindingResult, model)) {
+            if (saveExcursion(excursionForm,
+                    excursion -> excursionService.create(excursion),
+                    model))
+                return "redirect:/travel_agent/excursions?seaportId=" + excursionForm.getSeaportId();
         }
 
         setModelAttributesForExcursionForm(model, excursionForm);
@@ -157,4 +114,42 @@ public class TravelAgentExcursionsController {
         model.addAttribute("excursion", excursionForm);
         model.addAttribute("all_seaports", seaportService.getAllPorts());
     }
+
+    private boolean hasNoValidationErrors(BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            LOGGER.error("Validation error: " + bindingResult.getFieldErrors());
+            model.addAttribute("validation_errors", true);
+            return false;
+        }
+        return true;
+    }
+
+    private boolean saveExcursion(ExcursionForm excursionForm,
+                                  ThrowingConsumer<Excursion> savingMethod,
+                                  Model model) {
+        Excursion excursion = new ExcursionFormMapper().mapToEntity(excursionForm);
+        Long portId = excursionForm.getSeaportId();
+
+        try {
+            excursion.setSeaport(seaportService.getPortById(portId));
+            savingMethod.accept(excursion);
+            return true;
+
+        } catch (NoEntityFoundException e) {
+            LOGGER.error(e.getMessage());
+            model.addAttribute("no_port_found", true);
+        } catch (NonUniqueObjectException e) {
+            LOGGER.error(e.getMessage());
+            model.addAttribute("non_unique", true);
+        } catch (Throwable e) {
+            LOGGER.error(e.getMessage());
+            model.addAttribute("error", true);
+        }
+        return false;
+    }
+}
+
+@FunctionalInterface
+interface ThrowingConsumer<T> {
+    void accept(T var1) throws Throwable;
 }
