7144b67ae60d80c64ff1b5682422cf7225c3e82b
Olena Stoliarova <stoliarova.elena@gmail.com>
2020-04-19 14:00:32 +0300

    refactor ExtraService and ShipService

--- src/main/java/ua/cruise/company/service/ShipService.java
+++ src/main/java/ua/cruise/company/service/ShipService.java
@@ -8,17 +8,19 @@ import org.springframework.stereotype.Service;
 import ua.cruise.company.entity.Ship;
 import ua.cruise.company.repository.ShipRepository;
 import ua.cruise.company.service.exception.NoEntityFoundException;
+import ua.cruise.company.service.exception.NonUniqueObjectException;
+import ua.cruise.company.service.exception.SomethingWentWrongException;
 
 @Service
 public class ShipService {
-    private static final Logger LOGGER= LoggerFactory.getLogger(ShipService.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(ShipService.class);
 
     @Autowired
     private ShipRepository shipRepository;
 
     public Ship getShipById(Long id) throws NoEntityFoundException {
         return shipRepository.findById(id)
-                            .orElseThrow(() -> new NoEntityFoundException("There is no ship with provided id (" + id + ")"));
+                .orElseThrow(() -> new NoEntityFoundException("There is no ship with provided id (" + id + ")"));
     }
 
     public Ship getShipByName(String name) throws NoEntityFoundException {
@@ -26,13 +28,18 @@ public class ShipService {
                 .orElseThrow(() -> new NoEntityFoundException("There is no ship with provided name (" + name + ")"));
     }
 
-    public boolean saveShip(Ship ship){
-        try{
-            shipRepository.save(ship);
-        }catch (DataIntegrityViolationException exception){
+    public void create(Ship ship) throws NonUniqueObjectException, SomethingWentWrongException {
+        try {
+            ship = shipRepository.save(ship);
+
+            if (ship.getId() == null)
+                throw new SomethingWentWrongException("Ship wasn't saved");
+        } catch (DataIntegrityViolationException exception) {
             LOGGER.error("Ship wasn't saved {}, {}", ship, exception.getMessage());
-            return false;
+            throw new NonUniqueObjectException("Ship with such name already exists.");
+        } catch (Exception ex) {
+            LOGGER.error(ex.getMessage(), ex);
+            throw new SomethingWentWrongException(ex.getMessage(), ex);
         }
-        return true;
     }
 }
